7707730972a93b37e9a31341c927bb1bdb77625c
Diego López Torres <buyeye09@gmail.com>
2021-10-23 00:01:42 -0500

    Key prop added to Row list rendering

--- src/components/Table/Row.tsx
+++ src/components/Table/Row.tsx
@@ -10,8 +10,8 @@ const Row: FC<RowProps> = ({ data }) => {
 
     return (
         <tr>
-            { Object.values(data).map((value: any) => (
-                <td className="p-1 border border-blue-600">{ value }</td>
+            { Object.values(data).map((value: any, index: number) => (
+                <td key={ value + index } className="p-1 border border-blue-600">{ value }</td>
             )) }
             <td className="p-1 border border-blue-600">
                 <div className="flex justify-around gap-2 lg:gap-0">
