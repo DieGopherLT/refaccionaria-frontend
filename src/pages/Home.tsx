977cb2483d80544ca82ca57dbba95498a23efdae
Diego López Torres <buyeye09@gmail.com>
2021-11-11 16:18:57 -0600

    Sales are shown on UI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,67 @@
-import React, { FC, useEffect } from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import React, { FC, useContext } from 'react';
+import { Link, RouteComponentProps } from 'react-router-dom';
+
+import SaleContext from '../context/Sales/SaleContext';
 
 import PageContainer from '../components/UI/PageContainer';
-import Table from '../components/Table/Table';
+import Wrapper from '../components/UI/Wrapper';
+import Button from '../components/UI/Button';
 
-interface HomeProps extends RouteComponentProps {}
+const Home: FC<RouteComponentProps> = props => {
 
-const Home: FC<HomeProps> = () => {
-
-    useEffect(() => {
-        console.log('Entering on home page');
-        return () => {
-            console.log('Leaving home page')
-        }
-    }, []);
-
+    const { sales } = useContext(SaleContext);
 
     return (
         <PageContainer>
-            <Table data={ [] }>
-                <th className="p-1 border border-blue-600 lg:w-9">ID</th>
-                <th className="p-1 border border-blue-600 lg:w-12">Nombre</th>
-                <th className="p-1 border border-blue-600 lg:w-32">Descripcion</th>
-                <th className="p-1 border border-blue-600 lg:w-8">Precio</th>
-                <th className="p-1 border border-blue-600 lg:w-8">Cantidad</th>
-                <th className="p-1 border border-blue-600 lg:w-28">Acciones</th>
-            </Table>
+            <Wrapper className="max-w-5xl mt-5">
+                <Link
+                    to="/ventas/nuevo"
+                    className="text-white text-center bg-green-500 hover:bg-green-700 p-3 rounded block w-full lg:w-44"
+                >Agregar nueva venta</Link>
+            </Wrapper>
+            <Wrapper className="max-w-5xl mt-5 overflow-x-auto whitespace-nowrap md:whitespace-normal">
+                <table className="w-full border-collapse lg:table-fixed">
+                    <thead>
+                        <tr>
+                            <th className="p-1 border border-blue-600 lg:w-14">Producto</th>
+                            <th className="p-1 border border-blue-600 lg:w-14">Marca</th>
+                            <th className="p-1 border border-blue-600 lg:w-14">Cantidad</th>
+                            <th className="p-1 border border-blue-600 lg:w-14">Precio</th>
+                            <th className="p-1 border border-blue-600 lg:w-14">Total</th>
+                            <th className="p-1 border border-blue-600 lg:w-28">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { sales.map((sale, index) => (
+                            <tr key={ index + sale.sale_id }>
+                                <td className="p-1 border border-blue-600">{ sale.product.name }</td>
+                                <td className="p-1 border border-blue-600">{ sale.product.brand }</td>
+                                <td className="p-1 border border-blue-600">{ sale.amount }</td>
+                                <td className="p-1 border border-blue-600">{ sale.product.price }</td>
+                                <td className="p-1 border border-blue-600">{ sale.total }</td>
+                                <td className="p-1 border border-blue-600">
+                                    <div className="flex justify-around gap-2 lg:gap-0">
+                                        <div className="w-full md:w-28">
+                                            <Button
+                                                color="yellow"
+                                                type="button"
+                                                text="Editar"
+                                            />
+                                        </div>
+                                        <div className="w-full md:w-28">
+                                            <Button
+                                                color="red"
+                                                type="button"
+                                                text="Borrar"
+                                            />
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        )) }
+                    </tbody>
+                </table>
+            </Wrapper>
         </PageContainer>
     );
 };
