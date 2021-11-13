2e01da88716feafbfce7f3544a0ffb3185f53866
Diego López Torres <buyeye09@gmail.com>
2021-11-12 21:02:27 -0600

    Delivery context boilerplate is done

---
+++ src/context/Delivery/DeliveryReducer.ts
@@ -0,0 +1,13 @@
+export type DeliveryReducerState = {
+
+}
+
+type DeliveryAction =
+    | { type: '' }
+
+export default function reducer(state: DeliveryReducerState, action: DeliveryAction): DeliveryReducerState {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
