45b9d5cf671f241b437fcf77569945e29776c2b9
Diego López Torres <buyeye09@gmail.com>
2021-11-19 02:11:37 -0600

    Logic to get total generated by sales is done

--- src/pages/SaleSummary.tsx
+++ src/pages/SaleSummary.tsx
@@ -8,7 +8,8 @@ import Wrapper from '../components/UI/Wrapper';
 
 const SaleSummary = () => {
 
-    const [mostSoldCategory, setMostSoldCategory] = useState<string>();
+    const [bestSellingCategory, setBestSellingCategory] = useState<string>();
+    const [totalGeneratedBySales, setTotalGeneratedBySales] = useState<number>();
 
     const { sales } = useContext(SaleContext);
     const { products } = useContext(ProductContext);
@@ -18,30 +19,39 @@ const SaleSummary = () => {
             const dictionary: {[key: string]: number} = {}
 
             products.forEach(product => {
-                let saleTotalAmount = 0;
+                let salesTotalAmount = 0;
                 const salesPerProduct = sales.filter(sale => sale.product.product_id === product.product_id)
                     .map(sale => sale.total)
 
                 if (salesPerProduct.length > 0) {
-                    saleTotalAmount = salesPerProduct.reduce((a, b) => a + b);
+                    salesTotalAmount = salesPerProduct.reduce((a, b) => a + b);
                 }
 
                 if (dictionary[product.category.name] in dictionary)
-                    dictionary[product.category.name] += saleTotalAmount;
+                    dictionary[product.category.name] += salesTotalAmount;
                 else
-                    dictionary[product.category.name] = saleTotalAmount;
+                    dictionary[product.category.name] = salesTotalAmount;
             });
 
             const maxValue = Math.max(...Object.values(dictionary));
-            const mostSoldCategory = Object.keys(dictionary).find(key => dictionary[key] === maxValue);
-            setMostSoldCategory(mostSoldCategory);
+            const bestSellingCategory = Object.keys(dictionary).find(key => dictionary[key] === maxValue);
+            setBestSellingCategory(bestSellingCategory);
         }
     }, [products, sales]);
+
+    useEffect(() => {
+        let total = 0;
+        if (sales.length > 0) {
+            total = sales.map(sale => sale.total).reduce((a, b) => a + b);
+        }
+        setTotalGeneratedBySales(total);
+    }, [sales]);
 
     return (
         <PageContainer>
             <Wrapper className="max-w-5xl mt-5">
-                <p>La categoria que tiene mas ventas es... { mostSoldCategory }</p>
+                <p>La categoria que tiene mas ventas es... { bestSellingCategory }</p>
+                <p>El total de ingresos generados es... { totalGeneratedBySales }</p>
             </Wrapper>
         </PageContainer>
     );
