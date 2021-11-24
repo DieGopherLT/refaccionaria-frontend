7e26a83d708e024aa7cbd55bf3668776e65356d2
Diego López Torres <buyeye09@gmail.com>
2021-11-24 00:54:36 -0600

    Added safelist to tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,25 @@
 module.exports = {
-  purge: ['./src/components/**/*.tsx', './src/pages/**/*.tsx', './src/App.tsx'],
+  purge: {
+      content: ['./src/components/**/*.tsx', './src/pages/**/*.tsx', './src/App.tsx'],
+      safelist: [
+          'bg-blue-500',
+          'hover:bg-blue-700',
+          'bg-green-500',
+          'hover:bg-green-700',
+          'bg-red-500',
+          'hover:bg-red-700',
+          'bg-gray-500',
+          'hover:bg-gray-700',
+          'bg-yellow-500',
+          'hover:bg-yellow-700',
+          'bg-indigo-500',
+          'hover:bg-indigo-700',
+          'bg-pink-500',
+          'hover:bg-pink-700',
+          'bg-purple-500',
+          'hover:bg-purple-700',
+      ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
