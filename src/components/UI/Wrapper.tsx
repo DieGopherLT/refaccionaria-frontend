8c66cb6f807fdfe9ac0bb2433f61df5b3111e50b
Diego López Torres <buyeye09@gmail.com>
2021-11-26 20:30:38 -0600

    Wrapper component width is now dependant on a prop

--- src/components/UI/Wrapper.tsx
+++ src/components/UI/Wrapper.tsx
@@ -2,10 +2,11 @@ import React, { FC } from 'react';
 
 interface WrapperProps {
     className?: string;
+    fullWidth?: boolean;
 }
 
-const Wrapper: FC<WrapperProps> = ({ children, className }) => {
-    const classes = `mx-auto w-11/12 lg:w-full ${className ? className : ''}`;
+const Wrapper: FC<WrapperProps> = ({ children, className, fullWidth = false }) => {
+    const classes = `mx-auto ${fullWidth ? 'w-full' : 'w-11/12'} ${className ? className : ''}`;
     return (
         <div className={ classes }>
             { children }
